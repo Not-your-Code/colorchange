ac3682a836b60ad8e4e2287eb9d11201756db935
Vineet Agarwal <vineetagarwal.now@gmail.com>
2021-09-23 15:00:43 +0530

    Inital Commit`

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let colors = ["Orangered","Blue","Yellow","Green","Orange", "Cyan","DodgerBlue", "Violet", "Navy", "Purple", "YellowGreen", "OrangeRed", "SlateBlue", "Salmon", "Crimson", "HotPink", "Magenta"];
-let ind = 0 ;
+var colors = ["Orangered","Blue","Yellow","Green","Orange", "Cyan","DodgerBlue", "Violet", "Navy", "Purple", "YellowGreen", "OrangeRed", "SlateBlue", "Salmon", "Crimson", "HotPink", "Magenta"];
+var index = 0 ;
 
-document.getElementById("#submit").addEventListener('click' ,()=>{
-    if(ind >  colors.length -1){
-        ind=0;
-        document.body.style.backgroundColor = colors[ind++];
-        }
+document.querySelector("#submit").addEventListener("click" ,()=>{
+    if(index >  colors.length -1)
+        index=0;
+    
+        document.body.style.backgroundColor = colors[index++];
+        
 });
